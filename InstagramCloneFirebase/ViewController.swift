25eb3899bfc898229c280f55f0bcad84493a3d51
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-08-29 22:42:04 +0300

    Initial Commit

---
+++ InstagramCloneFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramCloneFirebase
+//
+//  Created by Beyza Nur Tekerek on 29.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
