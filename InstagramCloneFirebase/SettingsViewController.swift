eff9ed2e3feda11f0f14e8fb0925a0446575ab1b
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-09-10 00:33:19 +0300

    first commit

---
+++ InstagramCloneFirebase/SettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.swift
+//  InstagramCloneFirebase
+//
+//  Created by Beyza Nur Tekerek on 30.08.2024.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logOutClicked(_ sender: Any) {
+
+        do {
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toViewController", sender: nil)
+        } catch {
+            print("error")
+        }
+        
+    }
+    
+
+}
